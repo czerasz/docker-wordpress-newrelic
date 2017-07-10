5a68cb85b9ef3cb98078910e66badf83737d2882
Michal Czeraszkiewicz <contact@czerasz.com>
2017-07-11 01:50:48 +0200

    Fix missing entrypoint

--- 7.1/fpm/docker-entrypoint.sh
+++ 7.1/fpm/docker-entrypoint.sh
@@ -4,4 +4,4 @@
 NR_INSTALL_SILENT=true NR_INSTALL_PHPLIST="/usr/local/bin" NR_INSTALL_PATH="/usr/local/bin" /usr/bin/newrelic-install install
 
 # Continue with default image behaviour
-/entrypoint.sh php-fpm
+/usr/local/bin/docker-entrypoint.sh php-fpm
