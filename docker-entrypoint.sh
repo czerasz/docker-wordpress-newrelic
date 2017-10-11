34816b1af7edbae1914c3e816be155ad1794eed2
Michal Czeraszkiewicz <contact@czerasz.com>
2017-10-11 09:57:03 +0200

    Make NewRelic configuration file optional in favour of environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -z "$NEWRELIC_LICENSE" ] && [ ! -z "$NEWRELIC_APPNAME" ]; then
+  gomplate --file /templates/newrelic.ini --out /usr/local/etc/php/conf.d/newrelic.ini
+fi
+
 # Initialize NewRelic APM
 NR_INSTALL_SILENT=true NR_INSTALL_PHPLIST="/usr/local/bin" NR_INSTALL_PATH="/usr/local/bin" /usr/bin/newrelic-install install
 
